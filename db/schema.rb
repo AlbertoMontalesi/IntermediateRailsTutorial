2c05385433942aa3023fdd77895f48f8c7bd7f8a
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-12-23 20:15:50 +0700

    modify sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171220045648) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

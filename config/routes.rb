b653ac271c9ff241f6c90446969b2cefd0cd7282
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-12-25 17:01:17 +0700

    create post controller and render post on index page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
 end

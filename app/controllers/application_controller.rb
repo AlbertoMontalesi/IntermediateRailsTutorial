64f97af8a25c2965c2fa754148ecd3db65881291
AlbertoMontalesi <alberto.montalesi@outlook.it>
2018-01-03 19:55:16 +0700

    create ui for new posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def redirect_if_not_signed_in
+    redirect_to root_path unless user_signed_in?
+end
+
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+  
   def show
     @post = Post.find(params[:id])
   end
@@ -14,6 +18,23 @@ class PostsController < ApplicationController
   def team
     posts_for_branch(params[:action])
   end
+
+
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
 
   private
 
@@ -34,4 +55,12 @@ class PostsController < ApplicationController
       branch: params[:action]
     }).call
   end
+
+
+
+  def post_params
+  params.require(:post).permit(:content, :title, :category_id).merge(user_id: current_user.id)
+end
+
+
 end
